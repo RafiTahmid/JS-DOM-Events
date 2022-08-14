d123614d3c459c4c2920a2a121f5cab5b7c470b9
RafiTahmid <rafi.tahmid019@gmail.com>
2022-08-14 22:00:39 +0600

    Intro to DOM events

---
+++ js/events.js
@@ -0,0 +1,10 @@
+function makeRed(){
+    document.body.style.backgroundColor = "red";
+}
+
+const makeBlueButton = document. getElementById('make-blue');
+makeBlueButton. onclick = makeBlue;
+
+function makeBlue(){
+    document. body. style. backgroundColor = 'blue';
+}
